5546552334474be1869e9b255b39bc1e85dbfc22
johncaged <liuzikang0625@gmail.com>
2025-12-17 01:28:07 +0800

    feat: add key types; add generic

--- src/slyme/context/__init__.py
+++ src/slyme/context/__init__.py
@@ -1,11 +1,18 @@
 from slyme.utils.store import Store, Key
+from slyme.utils.typing import TypeVar
+
+_T = TypeVar("_T")
 
 
-class RequiresKey(Key):
+class RequiresKey(Key[_T]):
     pass
 
 
-class ProducesKey(Key):
+class ProducesKey(Key[_T]):
+    pass
+
+
+class RequiresProducesKey(RequiresKey[_T], ProducesKey[_T]):
     pass
 
 
