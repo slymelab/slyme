0a21c9fe7db38bd47268d0722e2249e8b2cf3ba5
johncaged <liuzikang0625@gmail.com>
2026-03-29 19:52:38 +0800

    misc: org rename.

--- src/slyme/context/core.py
+++ src/slyme/context/core.py
@@ -1,4 +1,4 @@
-# Copyright 2026 Slymer-Tech
+# Copyright 2026 The SlymeLab Team
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
